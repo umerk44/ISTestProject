6236e2a502eae3d4d6aca67d0dbae44a6df78c55
Muhammad Umer <umerk44@gmail.com>
2021-01-25 15:38:20 +0500

    Removed ISServiceBuilder

--- app/src/main/java/com/test/istestprojectapplication/network/ISServiceBuilder.kt
+++
@@ -1,7 +0,0 @@
-package com.test.istestprojectapplication.network
-
-import com.test.istestprojectapplication.core.SessionManager
-
-class ISServiceBuilder(private val sessionManager: SessionManager) {
-
-}
